4dacef00bc1ed1f08b23556cce7e51a8cac5d959
Eduardo <72766914+Lal0Land@users.noreply.github.com>
2022-10-16 21:08:32 -0300

    Tarea 1
    Dejo una solución a la tarea propuesta en el modulo 2

---
+++ Tarea/Tarea.py
@@ -0,0 +1,38 @@
+#Modulo 2
+#Tarea Final
+#Eduardo Galdames
+#16 Oct
+
+import random
+import time
+#Datos
+
+t_ventat = int(input("Ingresa Ventana de medición en seg: "))
+t_muestreo = int(input("Ingresa periodo de muestreo en seg: "))
+spT = int(input("Set Point de Tempertatura en [ºC]:  "))
+dt = 0  #contador de tiempo
+Calefactor = True
+
+#
+T_k = random.randint(0, 35) #temperatura actual
+print('Temperatura inicial : {} ºC'.format(T_k))
+Log = open('log_Tinit_{}_Tcalefactor_{}_tTotal_{}_tMuestreo_{}.csv'.format(T_k,spT,t_ventat,t_muestreo),'w')
+
+while dt < t_ventat:
+    deltaT = round(random.uniform(0, 2), 2)  # delta de temp
+
+    if Calefactor:
+        T_k += deltaT
+    else:
+        T_k -= deltaT
+
+    if T_k < spT:
+        Calefactor = True
+    else:
+        Calefactor = False
+    print(round(T_k,2), Calefactor)
+    dt += t_muestreo
+    Log.write('{};{}\n'.format(round(T_k,2),Calefactor))
+    time.sleep(t_muestreo)
+print('Fin de medición')
+Log.close()
