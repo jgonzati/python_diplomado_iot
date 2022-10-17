d0482f9d6e6e4e8317c5d622aef1e1d33a294c01
Eduardo <72766914+Lal0Land@users.noreply.github.com>
2022-10-16 21:10:36 -0300

    Update Tarea.py

--- Tarea/Tarea.py
+++ Tarea/Tarea.py
@@ -1,13 +1,13 @@
 #Modulo 2
 #Tarea Final
-#Eduardo Galdames
+#Grupo 5
 #16 Oct
 
 import random
 import time
 #Datos
 
-t_ventat = int(input("Ingresa Ventana de medición en seg: "))
+t_ventana = int(input("Ingresa Ventana de medición en seg: "))
 t_muestreo = int(input("Ingresa periodo de muestreo en seg: "))
 spT = int(input("Set Point de Tempertatura en [ºC]:  "))
 dt = 0  #contador de tiempo
@@ -16,9 +16,9 @@ Calefactor = True
 #
 T_k = random.randint(0, 35) #temperatura actual
 print('Temperatura inicial : {} ºC'.format(T_k))
-Log = open('log_Tinit_{}_Tcalefactor_{}_tTotal_{}_tMuestreo_{}.csv'.format(T_k,spT,t_ventat,t_muestreo),'w')
+Log = open('log_Tinit_{}_Tcalefactor_{}_tTotal_{}_tMuestreo_{}.csv'.format(T_k,spT,t_ventana,t_muestreo),'w')
 
-while dt < t_ventat:
+while dt < t_ventana:
     deltaT = round(random.uniform(0, 2), 2)  # delta de temp
 
     if Calefactor:
